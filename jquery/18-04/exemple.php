0a900ba54594c7e21802ebf05c15a833b610a96b
anna-sorts <anna.senen@gmail.com>
2017-04-18 14:36:38 +0200

    18 d'abril, json, ajax, php amb jquery

---
+++ jquery/18-04/exemple.php
@@ -0,0 +1,12 @@
+<?php
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		// collect value of input field
+		$num1 = $_POST['num1'];
+		$num2 = $_POST['num2'];
+		//echo "hola";
+		echo $num1 + $num2;
+	}
+	else {
+		echo "Aquest programa nomes funciona amb el metode POST";
+	}
+?>
